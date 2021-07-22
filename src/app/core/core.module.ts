0967c0fe356e03c7d17ce34615bcaf05e25c6030
Mathieu Montjarret <mathieu.montjarret@icloud.com>
2021-07-22 17:06:28 +0200

    etape 14.2 imports modules and coremodule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UiModule } from '../ui/ui.module';
+import { TemplateModule } from '../template/template.module';
+import { IconsModule } from '../icons/icons.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+  ],
+  exports: [
+    UiModule, TemplateModule, IconsModule
   ]
 })
 export class CoreModule { }
