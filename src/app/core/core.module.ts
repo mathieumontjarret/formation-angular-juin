83efd854f5c4f9651e3ed880a1f40ab89c80289f
Mathieu Montjarret <mathieu.montjarret@icloud.com>
2021-08-01 19:17:43 +0200

    étape 7 header, nav, footer and multiple transclusion

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,16 +3,23 @@ import { CommonModule } from '@angular/common';
 import { UiModule } from '../ui/ui.module';
 import { TemplateModule } from '../template/template.module';
 import { IconsModule } from '../icons/icons.module';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    NavComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule,
   ],
   exports: [
-    UiModule, TemplateModule, IconsModule
+    UiModule, TemplateModule, IconsModule, HeaderComponent, NavComponent, FooterComponent
   ]
 })
 export class CoreModule { }
