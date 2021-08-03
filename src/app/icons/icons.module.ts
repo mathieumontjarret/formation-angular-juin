8b64c0ce8629b9ff91ccfae5510a80767aef0713
Mathieu Montjarret <mathieu.montjarret@icloud.com>
2021-08-03 14:31:58 +0200

    étape 9 : install Fontawesome, generate icons components and use in app-component.html

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconCloseComponent } from './components/icon-close/icon-close.component';
+import { IconNavComponent } from './components/icon-nav/icon-nav.component';
+import { IconEditComponent } from './components/icon-edit/icon-edit.component';
+import { IconDeleteComponent } from './components/icon-delete/icon-delete.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    IconCloseComponent,
+    IconNavComponent,
+    IconEditComponent,
+    IconDeleteComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule
+  ],
+  exports: [
+    IconCloseComponent,
+    IconNavComponent,
+    IconEditComponent,
+    IconDeleteComponent
   ]
 })
 export class IconsModule { }
