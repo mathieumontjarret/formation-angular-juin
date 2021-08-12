7728fdb04a463b49a3184ce869664584270a7a10
Mathieu Montjarret <mathieu.montjarret@icloud.com>
2021-08-12 17:05:57 +0200

    étape 16-2 routes orders, clients et page not found

--- src/app/clients/clients-routing.module.ts
+++ src/app/clients/clients-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageAddClientsComponent } from './pages/page-add-clients/page-add-clients.component';
+import { PageEditClientsComponent } from './pages/page-edit-clients/page-edit-clients.component';
+import { PageListClientsComponent } from './pages/page-list-clients/page-list-clients.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PageListClientsComponent},
+  {path: 'add', component: PageAddClientsComponent},
+  {path: 'edit', component: PageEditClientsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/orders/orders-routing.module.ts
+++ src/app/orders/orders-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageAddOrdersComponent } from './pages/page-add-orders/page-add-orders.component';
+import { PageEditOrdersComponent } from './pages/page-edit-orders/page-edit-orders.component';
 import { PageListOrdersComponent } from './pages/page-list-orders/page-list-orders.component';
 
 const routes: Routes = [
   {path: '', component: PageListOrdersComponent},
+  {path: 'add', component: PageAddOrdersComponent},
+  {path: 'edit', component: PageEditOrdersComponent},
 
 ];
 
