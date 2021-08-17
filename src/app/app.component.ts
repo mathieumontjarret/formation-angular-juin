8c3df77f7877a71f9e68f3f0cddca427d25a05d6
Mathieu Montjarret <mathieu.montjarret@icloud.com>
2021-08-17 15:20:14 +0200

    étape 21: Exemple Observable, Subject et BehaviorSubject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 /**
  * this class is component who bootstrap application
  */
@@ -11,5 +12,40 @@ export class AppComponent {
   /**
    * this property is an exemple for interpolation in html
    */
-  title = 'formation-angular-juin';
+   title = 'formation-angular-juin';
+
+  private obs = new Observable((listXsubscribe) => {
+    listXsubscribe.next(Math.random());
+  });
+
+  private sub = new Subject();
+  private beh = new BehaviorSubject(0);
+
+  constructor(){
+    this.obs.subscribe((data) => {
+      console.log(data);
+    })
+    this.obs.subscribe((data) => {
+      console.log(data);
+    })
+    this.sub.next('test');
+    this.sub.next('test 2');
+    this.sub.subscribe((data) => {
+      console.log(data);
+    })
+    this.sub.subscribe((data) => {
+      console.log(data);
+    })
+    this.sub.next(Math.random());
+    this.sub.subscribe((data) => {
+      console.log(data);
+    })
+    this.beh.next(Math.random());
+    this.beh.subscribe((data) => {
+      console.log(data);
+    })
+    this.beh.subscribe((data) => {
+      console.log(data);
+    })
+  }
 }
