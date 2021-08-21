def73687a5e9e29b4f5a3265b1e1efd8a54679d2
Mathieu Montjarret <mathieu.montjarret@icloud.com>
2021-08-21 15:11:46 +0200

    étape 25: components stupides avec property binding et lifecycles

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TemplateModule } from '../template/template.module';
 import { IconsModule } from '../icons/icons.module';
+import { TableLightComponent } from './components/table-light/table-light.component';
+import { BtnComponent } from './components/btn/btn.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TableLightComponent,
+    BtnComponent
+  ],
   imports: [
     CommonModule
   ],
   exports: [
     TemplateModule,
-    IconsModule
+    IconsModule,
+    TableLightComponent,
+    BtnComponent
   ]
 })
 export class SharedModule { }
