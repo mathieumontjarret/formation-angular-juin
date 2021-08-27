c57824abdffc7c655cebf6027822604578d4b1d1
Mathieu Montjarret <mathieu.montjarret@icloud.com>
2021-08-27 18:13:30 +0200

    étape 30: Btn component with 2 @Input()

--- src/app/shared/components/btn/btn.component.ts
+++ src/app/shared/components/btn/btn.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-btn',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./btn.component.scss']
 })
 export class BtnComponent implements OnInit {
-
+  @Input() public label!: string;
+  @Input() public route!: string;
   constructor() { }
 
   ngOnInit(): void {
